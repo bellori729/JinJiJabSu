8d10e982b1cf98d5e125f0dd5207424e0d0cddcd
bellori729 <bellori729@gmail.com>
2025-02-02 22:16:34 +0900

    feat : 헤더에 텍스트 로고 컴포넌트 추가

---
+++ src/components/atoms/TextLogo.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom"
+import route from "../../router/route"
+
+const TextLogo = ({ className, style }) => {
+  const navigate = useNavigate()
+  return (
+    <img
+      className={className ? className : `w-[128px] h-[30px] hover:scale-[1.05] transition-transform cursor-pointer`}
+      style={style}
+      src={"/assets/images/text_logo.png"}
+      alt={"홈으로 이동"}
+      title={"홈으로 이동"}
+      onClick={() => navigate(route.home)}
+    />
+  )
+}
+
+export default TextLogo
--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,14 +1,15 @@
+import TextLogo from "../atoms/TextLogo"
 import BackBtn from "../molecules/BackBtn"
 import SearchBtn from "../molecules/SearchBtn"
 
-const Header = ({ isNone, isNoBack, isNoSearch }) => {
+const Header = ({ isNone, isNoBack, isNoLogo, isNoSearch }) => {
   return (
-    <header className="w-full h-[50px] flex items-center justify-between px-[20px]">
+    <header className={`w-full h-[50px] flex items-center px-[20px] justify-between`}>
       {!isNone && (
         <>
-          {!isNoBack && <BackBtn />}
-
-          {!isNoSearch && <SearchBtn />}
+          {!isNoBack ? <BackBtn /> : <div className="w-[30px] h-[30px]"></div>}
+          {!isNoLogo && <TextLogo />}
+          {!isNoSearch ? <SearchBtn /> : <div className="w-[30px] h-[30px]"></div>}
         </>
       )}
     </header>
