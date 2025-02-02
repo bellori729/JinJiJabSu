7cede0795824c8762f37847902094e12b8040905
bellori729 <bellori729@gmail.com>
2025-02-02 22:17:09 +0900

    refactor : 네비게이션 아이템 추가

--- src/components/pages/etc/UI.jsx
+++ src/components/pages/etc/UI.jsx
@@ -3,21 +3,35 @@ import MainContainer from "../../templates/MainContainer"
 import NavIcon from "../../atoms/NavIcon"
 import siGunLogoPath from "../../../lib/constants/siGunLogoPath"
 import SiGunLogo from "../../atoms/SiGunLogo"
+import NavItem from "../../molecules/NavItem"
 
 const UI = () => {
   return (
     <BasicTemplate>
+      <div className="h-[500px] w-full">dd</div>
       <MainContainer>
-        <NavIcon type="home" />
-        <NavIcon type="home_active" />
-        <NavIcon type="search" />
-        <NavIcon type="search_active" />
-        <NavIcon type="bookmark" />
-        <NavIcon type="bookmark_active" />
-        <NavIcon type="more" />
-        <NavIcon type="more_active" />
-        <NavIcon type="more_activedd" />
-        <div className="flex flex-col gap-[5px]">
+        <div className="w-full flex items-center justify-around">
+          <NavItem isHome />
+          <NavItem isHomeActive />
+          <NavItem isSearch />
+          <NavItem isSearchActive />
+          <NavItem isBookmark />
+          <NavItem isBookmarkActive />
+          <NavItem isMore />
+          <NavItem isMoreActive />
+        </div>
+        <div className="w-full flex items-center justify-around">
+          <NavIcon type="home" />
+          <NavIcon type="home_active" />
+          <NavIcon type="search" />
+          <NavIcon type="search_active" />
+          <NavIcon type="bookmark" />
+          <NavIcon type="bookmark_active" />
+          <NavIcon type="more" />
+          <NavIcon type="more_active" />
+          <NavIcon type="more_activedd" />
+        </div>
+        <div className="w-full flex flex-col items-center gap-[5px]">
           {Object.keys(siGunLogoPath).map((key) => {
             return <SiGunLogo key={key} path={siGunLogoPath[key]} alt={key} />
           })}
