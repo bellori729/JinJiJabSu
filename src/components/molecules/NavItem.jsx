107d80b4e78819147ac9980ccba293b37368383b
bellori729 <bellori729@gmail.com>
2025-02-02 22:47:31 +0900

    refactor : 액티브 이미지 프리로드

--- src/components/molecules/NavItem.jsx
+++ src/components/molecules/NavItem.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import route from "../../router/route"
 import NavIcon from "../atoms/NavIcon"
@@ -21,82 +22,69 @@ const NavItem = ({
       title: "홈",
       ariaText: "홈 페이지 이동",
       path: route.home,
+      activeImg: "/assets/icons/home_active.png",
     },
     search: {
       title: "검색",
       ariaText: "검색 페이지 이동",
       path: route.search,
+      activeImg: "/assets/icons/search_active.png",
     },
     bookmark: {
       title: "즐겨찾기",
       ariaText: "즐겨찾기 페이지 이동",
       path: route.bookmark,
+      activeImg: "/assets/icons/bookmark_active.png",
     },
     more: {
       title: "더보기",
       ariaText: "더보기 페이지 이동",
       path: route.more,
+      activeImg: "/assets/icons/more_active.png",
     },
   }
 
+  // 이미지 프리로드 함수
+  useEffect(() => {
+    Object.values(navItemInfo).forEach((item) => {
+      const img = new Image()
+      img.src = item.activeImg
+    })
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   const getTitle = () => {
-    if (isHome || isHomeActive) {
-      return navItemInfo.home.title
-    } else if (isSearch || isSearchActive) {
-      return navItemInfo.search.title
-    } else if (isBookmark || isBookmarkActive) {
-      return navItemInfo.bookmark.title
-    } else if (isMore || isMoreActive) {
-      return navItemInfo.more.title
-    }
+    if (isHome || isHomeActive) return navItemInfo.home.title
+    if (isSearch || isSearchActive) return navItemInfo.search.title
+    if (isBookmark || isBookmarkActive) return navItemInfo.bookmark.title
+    if (isMore || isMoreActive) return navItemInfo.more.title
   }
 
   const getPath = () => {
-    if (isHome || isHomeActive) {
-      return navItemInfo.home.path
-    } else if (isSearch || isSearchActive) {
-      return navItemInfo.search.path
-    } else if (isBookmark || isBookmarkActive) {
-      return navItemInfo.bookmark.path
-    } else if (isMore || isMoreActive) {
-      return navItemInfo.more.path
-    }
+    if (isHome || isHomeActive) return navItemInfo.home.path
+    if (isSearch || isSearchActive) return navItemInfo.search.path
+    if (isBookmark || isBookmarkActive) return navItemInfo.bookmark.path
+    if (isMore || isMoreActive) return navItemInfo.more.path
   }
 
   const getAriaText = () => {
-    if (isHome || isHomeActive) {
-      return navItemInfo.home.ariaText
-    } else if (isSearch || isSearchActive) {
-      return navItemInfo.search.ariaText
-    } else if (isBookmark || isBookmarkActive) {
-      return navItemInfo.bookmark.ariaText
-    } else if (isMore || isMoreActive) {
-      return navItemInfo.more.ariaText
-    }
+    if (isHome || isHomeActive) return navItemInfo.home.ariaText
+    if (isSearch || isSearchActive) return navItemInfo.search.ariaText
+    if (isBookmark || isBookmarkActive) return navItemInfo.bookmark.ariaText
+    if (isMore || isMoreActive) return navItemInfo.more.ariaText
   }
 
   const getType = () => {
-    if (isHome || isHomeActive) {
-      return "home"
-    } else if (isSearch || isSearchActive) {
-      return "search"
-    } else if (isBookmark || isBookmarkActive) {
-      return "bookmark"
-    } else if (isMore || isMoreActive) {
-      return "more"
-    }
+    if (isHome || isHomeActive) return "home"
+    if (isSearch || isSearchActive) return "search"
+    if (isBookmark || isBookmarkActive) return "bookmark"
+    if (isMore || isMoreActive) return "more"
   }
 
   const getIsActive = () => {
-    if (isHomeActive) {
-      return "home_active"
-    } else if (isSearchActive) {
-      return "search_active"
-    } else if (isBookmarkActive) {
-      return "bookmark_active"
-    } else if (isMoreActive) {
-      return "more_active"
-    }
+    if (isHomeActive) return "home_active"
+    if (isSearchActive) return "search_active"
+    if (isBookmarkActive) return "bookmark_active"
+    if (isMoreActive) return "more_active"
   }
 
   return (
@@ -105,10 +93,7 @@ const NavItem = ({
       aria-label={getAriaText()}
       title={getAriaText()}
       className="flex flex-col items-center justify-center hover:scale-[1.1] transition-transform"
-      onClick={() => {
-        const path = getPath()
-        navigate(path)
-      }}
+      onClick={() => navigate(getPath())}
     >
       <NavIcon type={getType()} isActive={getIsActive()} />
       <p className={`${basicTextBold} ${isActive && "!text-[#0A82AE]"}`}>{getTitle()}</p>
