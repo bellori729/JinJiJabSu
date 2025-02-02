51d93258e173ca9157e735e5c53fbab4c7c3a8ff
bellori729 <bellori729@gmail.com>
2025-02-02 19:40:32 +0900

    feat : 시/군 로고 컴포넌트 추가

--- src/components/atoms/SiGunLogo.jsx
+++ src/components/atoms/SiGunLogo.jsx
@@ -3,7 +3,7 @@ const SiGunLogo = ({ className, style, path, siGunName }) => {
 
   return (
     <img
-      className={className ? className : `w-[268px] h-[98px] bg-white`}
+      className={className ? className : `w-[268px] h-[98px] rounded-[8px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]`}
       style={style}
       src={path}
       alt={siGunName}
