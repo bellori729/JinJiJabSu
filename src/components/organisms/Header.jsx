694ef80cc4c491cd9998a435f5dd78daaf120696
bellori729 <bellori729@gmail.com>
2025-02-09 06:29:00 +0900

    feat : 헤더 텍스트가 있는 경우 스크린 리더기가 별도로 읽을 수 있도록 대체 텍스트 제공

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -13,7 +13,14 @@ const Header = ({ isNone, isNoBack, isNoLogo, isSquareLogo, isNoSearch, headerTe
           {!isNoLogo && <TextLogo />}
           {headerText && (
             <>
-              <p className="text-[#282828] text-[30px] leading-[100%] font-bold">{headerText}</p>
+              <span className="sr-only">이곳은 {headerText} 페이지입니다.</span>
+
+              <p
+                aria-hidden="true"
+                className="text-[#282828] text-[30px] leading-[100%] font-bold hover:scale-[1.05] transition-transform cursor-default"
+              >
+                {headerText}
+              </p>
             </>
           )}
           {!isNoSearch ? <SearchBtn /> : <div className="w-[30px] h-[30px]"></div>}
