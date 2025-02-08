d1986b31421c34e8a9c3d1c9eee74e3c86e31260
bellori729 <bellori729@gmail.com>
2025-02-09 06:28:23 +0900

    refactor : 이미지 가로 세로 비율 유지 및 부모 요소로 30px * 30px 설정

--- src/components/atoms/TextLogoSquare.jsx
+++ src/components/atoms/TextLogoSquare.jsx
@@ -5,14 +5,18 @@ const TextLogoSquare = ({ className, style }) => {
   const navigate = useNavigate()
 
   return (
-    <img
-      className={className ? className : `w-[30px] h-[30px] hover:scale-[1.05] transition-transform cursor-pointer`}
+    <div
+      className={className ? className : `w-[28px] h-[30px] hover:scale-[1.05] transition-transform cursor-pointer`}
       style={style}
-      src={"/assets/images/text_logo_square.png"}
-      alt={"홈으로 이동"}
-      title={"홈으로 이동"}
-      onClick={() => navigate(route.home)}
-    />
+    >
+      <img
+        className={"w-[28px] h-[30px]"}
+        src={"/assets/images/text_logo_square.png"}
+        alt={"홈으로 이동"}
+        title={"홈으로 이동"}
+        onClick={() => navigate(route.home)}
+      />
+    </div>
   )
 }
 
