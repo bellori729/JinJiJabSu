c16ca916e69d3b4496e03e0aa916e462d39489e4
bellori729 <bellori729@gmail.com>
2025-02-09 04:24:39 +0900

    feat : 탠스택 쿼리 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css"
 import { useScrollToTop } from "./lib/hooks/useScrollToTop"
 import Router from "./router/Router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 const App = () => {
   useScrollToTop()
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router />
-    </>
+    </QueryClientProvider>
   )
 }
 
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,65 +1,48 @@
-import { useEffect, useState } from "react"
+import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 import { API_KEY, API_URL } from "../../../lib/api/apj"
 import BasicTemplate from "../../templates/BasicTemplate"
 import MainContainer from "../../templates/MainContainer"
 
+const getList = async () => {
+  const params = {
+    KEY: API_KEY,
+    Type: "json",
+    pIndex: 1,
+    pSize: 10,
+  }
+
+  try {
+    const response = await axios.get(API_URL, { params })
+    const data = response.data?.OdsnFreemlsvM
+
+    if (!data || !Array.isArray(data) || data.length < 2) {
+      throw new Error("올바른 응답 구조가 아닙니다.")
+    }
+
+    return {
+      totalCount: data[0]?.head?.[0]?.list_total_count || 0,
+      list: data[1]?.row || [],
+    }
+  } catch (error) {
+    console.error("API 호출 중 오류 발생:", error)
+    return { totalCount: 0, list: [] }
+  }
+}
+
 const Home = () => {
-  useEffect(() => {
-    const data = fetchData()
-
-    data.then((response) => {
-      console.log(response)
-
-      getListTotalCount(response)
-      getList(response)
-    })
-  }, [])
-
-  const fetchData = async () => {
-    const data = {
-      KEY: API_KEY,
-      Type: "json",
-      pIndex: 1,
-      pSize: 10,
-    }
-    try {
-      const response = (
-        await axios.get(API_URL, {
-          params: data,
-        })
-      ).data.OdsnFreemlsvM
-
-      return response
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  // 총 결과 수
-  const [listTotalCount, setListTotalCount] = useState(0)
-
-  const getListTotalCount = (data) => {
-    const listTotalCount = data[0].head[0].list_total_count
-
-    setListTotalCount(listTotalCount)
-  }
-
-  // 리스트
-  const [list, setList] = useState([])
-
-  const getList = (data) => {
-    const list = data[1].row
-
-    setList(list)
-  }
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["list"],
+    queryFn: getList,
+    staleTime: 600000, // 600초 동안 캐시 유지
+  })
 
   return (
     <BasicTemplate isNoBack={true}>
-      <div>총 결과 {listTotalCount}</div>
+      <div>총 결과 {data.totalCount}</div>
       <MainContainer>
         <ul>
-          {list.map((item) => (
+          {data.list.map((item) => (
             <li key={item.MANAGE_INST_TELNO}>{item.FACLT_NM}</li>
           ))}
         </ul>
