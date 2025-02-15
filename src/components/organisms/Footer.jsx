d15775045d2a628c132e49cb2db803ed053ab569
bellori729 <bellori729@gmail.com>
2025-02-16 01:20:46 +0900

    style : 패딩 값 추가

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full h-[85px] py-[25px] flex items-center mt-[50px]">
+    <footer className="w-full h-[85px] py-[25px] px-[20px] flex items-center mt-[50px]">
       <ul>
         <li className="text-[16px] text-[#272829]">© BELLORI All rights reserved.</li>
       </ul>
