0870795162453dd9c34ea79c83e7ab90d29ba0fd
bellori729 <bellori729@gmail.com>
2025-02-16 01:25:50 +0900

    refactor : 이메일 추가

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Footer = () => {
   return (
     <footer className="w-full h-[85px] py-[25px] px-[20px] flex items-center mt-[50px]">
       <ul>
         <li className="text-[16px] text-[#272829]">© BELLORI All rights reserved.</li>
+        <li className="text-[16px] text-[#272829]">
+          <Link to="mailto:dhtldka@icloud.com">dhtldka@icloud.com</Link>
+        </li>
       </ul>
     </footer>
   )
