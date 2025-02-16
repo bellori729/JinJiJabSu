7899e3a2e1b408766b8101db63b794e71f749027
bellori729 <bellori729@gmail.com>
2025-02-16 13:47:32 +0900

    style : 너비 100vw -> 100%로 수정

--- src/components/templates/BodyContainer.jsx
+++ src/components/templates/BodyContainer.jsx
@@ -1,7 +1,7 @@
 const BodyContainer = ({ className, style, children }) => {
   return (
     <div
-      className={`w-[100vw] flex flex-col justify-center relative ${className}`}
+      className={`w-[100%] flex flex-col justify-center relative ${className}`}
       style={{
         ...style,
       }}
