823e6bc3021a0141353f1c252da90573b9afcbbf
bellori729 <bellori729@gmail.com>
2025-02-16 12:51:11 +0900

    refactor : - 시/군 그룹 이미지 요소 추가 - 무료급식소 찾기 버튼 영역 전체 요소 레이아웃 수정 - 각 이미지 요소 화면 크기에 따라 동적으로 크기 수정

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -12,7 +12,11 @@ const Home = () => {
 
   const [imagesLoaded, setImagesLoaded] = useState(false)
 
-  const imageUrls = ["/assets/images/phone.png", "/assets/images/uijeongbu_sijang.png"]
+  const imageUrls = [
+    "/assets/images/phone.png",
+    "/assets/images/si_gun_group.png",
+    "/assets/images/uijeongbu_sijang.png",
+  ]
 
   // 이미지 미리 로드 함수
   useEffect(() => {
@@ -38,12 +42,13 @@ const Home = () => {
     third: false,
     fourth: false,
     fifth: false,
+    sixth: false,
   })
 
   return (
     <BasicTemplate isNoBack={true}>
-      <MainContainer className={"px-[20px] h-[1318px]"}>
-        <div className="flex flex-col my-[40px]">
+      <MainContainer className={"h-fit min-h-[1663px]"}>
+        <div className="flex flex-col my-[40px] px-[20px]">
           <MoveUpAnimation className="large-font-size font-bold" durationTime={0.8} delayTime={0}>
             어르신!
           </MoveUpAnimation>
@@ -63,23 +68,25 @@ const Home = () => {
         </div>
 
         {animationController.first && (
-          <MoveUpOrderAnimationText
-            className="text-[#272829]"
-            text="진지잡수 는 경기도 내 어르신을 대상으로 하는 무료급식소 현황을 확인할 수 있는 서비스입니다."
-            highlightWords={{
-              진지잡수: "text-basic-blue font-bold", // 파란색 + 굵게
-              경기도: "text-basic-green font-bold", // 초록색 + 굵게
-            }}
-            onAnimationComplete={() => {
-              setAnimationController((prev) => {
-                return { ...prev, second: true }
-              })
-            }}
-          />
+          <div className="px-[20px]">
+            <MoveUpOrderAnimationText
+              className="text-[#272829]"
+              text="진지잡수 는 경기도 내 어르신을 대상으로 하는 무료급식소 현황을 확인할 수 있는 서비스입니다."
+              highlightWords={{
+                진지잡수: "text-basic-blue font-bold", // 파란색 + 굵게
+                경기도: "text-basic-green font-bold", // 초록색 + 굵게
+              }}
+              onAnimationComplete={() => {
+                setAnimationController((prev) => {
+                  return { ...prev, second: true }
+                })
+              }}
+            />
+          </div>
         )}
 
         {animationController.second && (
-          <div className="flex justify-end my-[40px]">
+          <div className="flex justify-end my-[40px] px-[20px]">
             {imagesLoaded && (
               <MoveUpAnimation
                 durationTime={0.8}
@@ -89,7 +96,11 @@ const Home = () => {
                   })
                 }}
               >
-                <img className="w-[187px] h-[261px]" src={imageUrls[0]} alt="phone" />
+                <img
+                  className="w-[187px] h-[261px] md:w-[374px] md:h-[522px] lg:w-[561px] lg:h-[783px]"
+                  src={imageUrls[0]}
+                  alt="phone"
+                />
               </MoveUpAnimation>
             )}
           </div>
@@ -107,7 +118,7 @@ const Home = () => {
           //   }}
           // />
           <MoveUpAnimation
-            className="text-[#272829]"
+            className="text-[#272829] px-[20px]"
             onAnimationComplete={() => {
               setAnimationController((prev) => {
                 return { ...prev, fourth: true }
@@ -118,8 +129,25 @@ const Home = () => {
           </MoveUpAnimation>
         )}
 
-        {animationController.fourth && (
-          <div className="flex mt-[20px]">
+        {animationController.fourth && imagesLoaded && (
+          <MoveUpAnimation
+            className="w-full h-auto flex justify-center mt-[40px] px-[20px]"
+            onAnimationComplete={() => {
+              setAnimationController((prev) => {
+                return { ...prev, fifth: true }
+              })
+            }}
+          >
+            <img
+              className="w-[320px] h-[285px] md:w-[640px] md:h-[570px] lg:w-[960px] lg:h-[855px]"
+              src={imageUrls[1]}
+              alt="경기도 산하 시군구가 모여있는 그룹 이미지"
+            />
+          </MoveUpAnimation>
+        )}
+
+        {animationController.fifth && (
+          <div className="flex mt-[40px]">
             {/* <MoveUpOrderAnimationText
               className="text-[#272829]"
               text="경기도 내 타 지역으로 가시더라도 검색을 통해 해당 지역의 무료 급식소를 확인하실 수 있습니다."
@@ -132,10 +160,10 @@ const Home = () => {
             /> */}
 
             <MoveUpAnimation
-              className="text-[#272829]"
+              className="text-[#272829] px-[20px]"
               onAnimationComplete={() => {
                 setAnimationController((prev) => {
-                  return { ...prev, fifth: true }
+                  return { ...prev, sixth: true }
                 })
               }}
             >
@@ -143,55 +171,54 @@ const Home = () => {
             </MoveUpAnimation>
           </div>
         )}
+        {animationController.sixth && imagesLoaded && (
+          <motion.div
+            className="w-full h-[490px] relative"
+            initial={{ opacity: 0 }}
+            // animate={{ opacity: 1 }}
+            whileInView={{ opacity: 1 }} // 화면에 보이면 나타나기
+            viewport={{ once: true, amount: 0.3 }} // 한 번만 애니메이션 (화면에 30% 들어오면 애니메이션)
+            transition={{ duration: 1.0, ease: "easeOut" }}
+          >
+            {/* 이미지 페이드인 */}
+            <img
+              className="w-full h-full object-cover"
+              src={imageUrls[2]}
+              alt="제33대 경기도 의정부시 김동근 시장이 어르신들에게 무료급식을 나눠주고 있습니다."
+            />
+
+            <MoveUpAnimation className="absolute top-0 left-0 w-full h-full pt-[171px] flex flex-col items-center gap-[45px] z-30">
+              <p className="text-[#272829] text-center large-font-size font-bold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+                진지 거르시지 마시고
+                <br />
+                꼭! 진지 잡수세요
+              </p>
+
+              <motion.button
+                className={`px-[26px] py-[13px] rounded-[20px] bg-basic-green text-white large-font-size font-bold shadow-[0px_4px_4px_rgba(0,0,0,0.25)]`}
+                animate={{
+                  y: [0, -10, 0], // 0 → 위로 10px → 다시 원래 위치 (반복)
+                }}
+                transition={{
+                  duration: 1,
+                  repeat: Infinity, // 무한 반복
+                  repeatType: "reverse", // 위아래 왕복 (부드러운 반복)
+                  ease: "easeInOut", // 부드러운 움직임
+                }}
+                onClick={() => {
+                  navigate(route.search)
+                }}
+              >
+                무료급식소 찾기
+              </motion.button>
+            </MoveUpAnimation>
+
+            {/* 그라디언트 오버레이 */}
+            <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#F8F9FA] to-transparent z-10" />
+            <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#0A82AE00] via-[#0A82AE4D] to-[#0A82AE99] z-20" />
+          </motion.div>
+        )}
       </MainContainer>
-
-      {animationController.fifth && imagesLoaded && (
-        <motion.div
-          className="absolute w-full h-[490px] bottom-[135px]"
-          initial={{ opacity: 0 }}
-          // animate={{ opacity: 1 }}
-          whileInView={{ opacity: 1 }} // 화면에 보이면 나타나기
-          viewport={{ once: true, amount: 0.3 }} // 한 번만 애니메이션 (화면에 30% 들어오면 애니메이션)
-          transition={{ duration: 1.0, ease: "easeOut" }}
-        >
-          {/* 이미지 페이드인 */}
-          <img
-            className="w-full h-full object-cover"
-            src={imageUrls[1]}
-            alt="제33대 경기도 의정부시 김동근 시장이 어르신들에게 무료급식을 나눠주고 있습니다."
-          />
-
-          <MoveUpAnimation className="absolute top-0 left-0 w-full h-full pt-[171px] flex flex-col items-center gap-[45px] z-30">
-            <p className="text-[#272829] text-center large-font-size font-bold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
-              진지 거르시지 마시고
-              <br />
-              꼭! 진지 잡수세요
-            </p>
-
-            <motion.button
-              className={`px-[26px] py-[13px] rounded-[20px] bg-basic-green text-white large-font-size font-bold shadow-[0px_4px_4px_rgba(0,0,0,0.25)]`}
-              animate={{
-                y: [0, -10, 0], // 0 → 위로 10px → 다시 원래 위치 (반복)
-              }}
-              transition={{
-                duration: 1,
-                repeat: Infinity, // 무한 반복
-                repeatType: "reverse", // 위아래 왕복 (부드러운 반복)
-                ease: "easeInOut", // 부드러운 움직임
-              }}
-              onClick={() => {
-                navigate(route.search)
-              }}
-            >
-              무료급식소 찾기
-            </motion.button>
-          </MoveUpAnimation>
-
-          {/* 그라디언트 오버레이 */}
-          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#F8F9FA] to-transparent z-10" />
-          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-[#0A82AE00] via-[#0A82AE4D] to-[#0A82AE99] z-20" />
-        </motion.div>
-      )}
     </BasicTemplate>
   )
 }
