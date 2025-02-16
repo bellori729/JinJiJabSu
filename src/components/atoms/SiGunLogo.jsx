c34df49eee484ca6c1729c2117293b37ed3757ce
bellori729 <bellori729@gmail.com>
2025-02-16 14:56:39 +0900

    style : 최소 너비 수정

--- src/components/atoms/SiGunLogo.jsx
+++ src/components/atoms/SiGunLogo.jsx
@@ -4,7 +4,7 @@ const SiGunLogo = ({ className, style, path, siGunName }) => {
       className={
         className
           ? className
-          : `w-[160px] sm:w-[160px] md:w-[200px] lg:w-[268px] rounded-[8px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]`
+          : `w-[152px] sm:w-[152px] md:w-[200px] lg:w-[268px] rounded-[8px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]`
       }
       style={style}
       src={path}
