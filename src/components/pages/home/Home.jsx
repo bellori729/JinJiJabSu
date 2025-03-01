ac9b860d5da3f3577595b4d05aa2c336bcdb7a81
bellori729 <bellori729@gmail.com>
2025-03-01 15:03:22 +0900

    refactor : 기다리지 않더라도 화면에 마운트 되면 요소 애니메이션 시작될 수 있도록 수정

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -36,94 +36,44 @@ const Home = () => {
     preloadImages()
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
-  const [animationController, setAnimationController] = useState({
-    first: false,
-    second: false,
-    third: false,
-    fourth: false,
-    fifth: false,
-    sixth: false,
-  })
-
   return (
     <BasicTemplate isNoBack={true}>
       <MainContainer className={"!min-h-[1528px] md:!min-h-[2074px] lg:!min-h-[2330px]"}>
         <div className="flex flex-col my-[40px] px-[20px]">
-          <MoveUpAnimation className="large-font-size font-bold" durationTime={0.8} delayTime={0}>
+          <MoveUpAnimation className="large-font-size font-bold" durationTime={0.8}>
             어르신!
           </MoveUpAnimation>
-
-          <MoveUpAnimation
-            className="large-font-size font-bold"
-            durationTime={0.8}
-            delayTime={0.8}
-            onAnimationComplete={() => {
-              setAnimationController((prev) => {
-                return { ...prev, first: true }
-              })
-            }}
-          >
+          <MoveUpAnimation className="large-font-size font-bold" durationTime={0.8} delayTime={0.8}>
             진지잡수~
           </MoveUpAnimation>
         </div>
 
-        {animationController.first && (
-          <div className="px-[20px]">
-            <MoveUpOrderAnimationText
-              className="text-[#272829]"
-              text="진지잡수 는 경기도 내 어르신을 대상으로 하는 무료급식소 현황을 확인할 수 있는 서비스입니다."
-              highlightWords={{
-                진지잡수: "text-basic-blue font-bold", // 파란색 + 굵게
-                경기도: "text-basic-green font-bold", // 초록색 + 굵게
-              }}
-              onAnimationComplete={() => {
-                setAnimationController((prev) => {
-                  return { ...prev, second: true }
-                })
-              }}
-            />
-          </div>
-        )}
-
-        {animationController.second && (
-          <div className="flex justify-end my-[40px] px-[20px]">
-            {imagesLoaded && (
-              <MoveUpAnimation
-                durationTime={0.8}
-                onAnimationComplete={() => {
-                  setAnimationController((prev) => {
-                    return { ...prev, third: true }
-                  })
-                }}
-              >
-                <img className="w-[187px] h-[261px] md:w-[374px] md:h-[522px]" src={imageUrls[0]} alt="phone" />
-              </MoveUpAnimation>
-            )}
-          </div>
-        )}
-
-        {animationController.third && (
-          <MoveUpAnimation
-            className="text-[#272829] px-[20px]"
-            onAnimationComplete={() => {
-              setAnimationController((prev) => {
-                return { ...prev, fourth: true }
-              })
+        <div className="px-[20px]">
+          <MoveUpOrderAnimationText
+            className="text-[#272829]"
+            delayTime={1.6}
+            text="진지잡수 는 경기도 내 어르신을 대상으로 하는 무료급식소 현황을 확인할 수 있는 서비스입니다."
+            highlightWords={{
+              진지잡수: "text-basic-blue font-bold", // 파란색 + 굵게
+              경기도: "text-basic-green font-bold", // 초록색 + 굵게
             }}
-          >
-            어르신께서 거주하시는 시/군에서 운영 중인 무료급식소의 정보를 한 눈에 확인하실 수 있어요!
-          </MoveUpAnimation>
-        )}
+          />
+        </div>
 
-        {animationController.fourth && imagesLoaded && (
-          <MoveUpAnimation
-            className="w-full h-auto flex justify-center mt-[40px] px-[20px]"
-            onAnimationComplete={() => {
-              setAnimationController((prev) => {
-                return { ...prev, fifth: true }
-              })
-            }}
-          >
+        <div className="flex justify-end my-[40px] px-[20px]">
+          {imagesLoaded && (
+            <MoveUpAnimation delayTime={0.3} durationTime={0.8} amountRatio={1}>
+              <img className="w-[187px] h-[261px] md:w-[374px] md:h-[522px]" src={imageUrls[0]} alt="phone" />
+            </MoveUpAnimation>
+          )}
+        </div>
+
+        <MoveUpAnimation className="text-[#272829] px-[20px]" amountRatio={1}>
+          어르신께서 거주하시는 시/군에서 운영 중인 무료급식소의 정보를 한 눈에 확인하실 수 있어요!
+        </MoveUpAnimation>
+
+        {imagesLoaded && (
+          <MoveUpAnimation className="w-full h-auto flex justify-center mt-[40px] px-[20px]" amountRatio={1}>
             <img
               className="w-[320px] h-[285px] md:w-[640px] md:h-[570px] lg:w-[960px] lg:h-[855px]"
               src={imageUrls[1]}
@@ -132,27 +82,17 @@ const Home = () => {
           </MoveUpAnimation>
         )}
 
-        {animationController.fifth && (
-          <div className="flex my-[40px]">
-            <MoveUpAnimation
-              className="text-[#272829] px-[20px]"
-              onAnimationComplete={() => {
-                setAnimationController((prev) => {
-                  return { ...prev, sixth: true }
-                })
-              }}
-            >
-              경기도 내 타 지역으로 가시더라도 검색을 통해 해당 지역의 무료 급식소를 확인하실 수 있습니다.
-            </MoveUpAnimation>
-          </div>
-        )}
-        {animationController.sixth && imagesLoaded && (
+        <div className="flex my-[40px]">
+          <MoveUpAnimation className="text-[#272829] px-[20px]" amountRatio={1}>
+            경기도 내 타 지역으로 가시더라도 검색을 통해 해당 지역의 무료 급식소를 확인하실 수 있습니다.
+          </MoveUpAnimation>
+        </div>
+        {imagesLoaded && (
           <motion.div
             className="w-full h-[490px] relative"
             initial={{ opacity: 0 }}
             // animate={{ opacity: 1 }}
             whileInView={{ opacity: 1 }} // 화면에 보이면 나타나기
-            viewport={{ once: true, amount: 0.3 }} // 한 번만 애니메이션 (화면에 30% 들어오면 애니메이션)
             transition={{ duration: 1.0, ease: "easeOut" }}
           >
             {/* 이미지 페이드인 */}
