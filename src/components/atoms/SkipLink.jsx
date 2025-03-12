706a64e0758c6ea369f8e86997d3a4ea98d96b77
bellori729 <bellori729@gmail.com>
2025-03-13 00:09:56 +0900

    style : - z index 값 조정 - 포커싱 안 되었을 때에는 화면 위로 숨겨놓기

--- src/components/atoms/SkipLink.jsx
+++ src/components/atoms/SkipLink.jsx
@@ -2,7 +2,7 @@ const SkipLink = ({ id, text }) => {
   return (
     <a
       href={`#${id}`}
-      className="sr-only focus-visible:not-sr-only focus-visible:absolute focus-visible:top-0 focus-visible:left-0 focus-visible:right-0 h-10 bg-[#272829] font-[700] w-full flex items-center justify-center text-white transition-all duration-300 ease-in-out"
+      className="z-100 absolute top-[-50px] focus-visible:top-0 focus-visible:left-0 focus-visible:right-0 h-10 bg-[#272829] font-[700] w-full flex items-center justify-center text-white transition-all duration-300 ease-in-out"
     >
       {text || "본문 바로가기"}
     </a>
